72d06ddfb873c37cfb8e20167f4623cc63963346
tomdaniels <thomsondaniels@hotmail.com>
2018-04-05 10:30:38 +1000

    message printing from addNote function

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ let command = argv._[0];
 
 if (command === 'add') {
   let note = notes.addNote(argv.title, argv.body);
+  if (note) {
+    console.log(`${note.title} has succsefully been added`);
+    console.log('-----------')
+    console.log(`Title: ${note.title}`);
+    console.log(`Body: ${note.body}`);
+  } else {
+    console.log(`there is already a note titled ${argv.title}`);
+  };
 } else if (command === 'list') {
   notes.getAll();
 } else if (command === 'read') {
