e8a236610f60b8c02be8787187335407ce9dd13d
tomdaniels <thomsondaniels@hotmail.com>
2018-04-11 09:39:01 +1000

    list notes function

--- notes.js
+++ notes.js
@@ -30,7 +30,7 @@ let addNote = (title, body) => {
 };
 
 let getAll = () => {
-  console.log('getting all notes');
+  return fetchNotes();
 };
 
 let getNote = (title) => {
@@ -48,6 +48,7 @@ let removeNote = (title) => {
 };
 
 let logNote = (note) => {
+  debugger;
   console.log('-----------')
   console.log(`Title: ${note.title}`);
   console.log(`Body: ${note.body}`);
